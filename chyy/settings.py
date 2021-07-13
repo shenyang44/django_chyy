0a0decf0cfe21965d96f9905cf567aa1cb0bc735
Phoong Shen Yang <shenyang.zx@gmail.com>
2021-07-13 15:56:39 +0800

    allowed hosts

--- chyy/settings.py
+++ chyy/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/ledger/static/ledger/'
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
