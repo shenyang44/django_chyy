00ab9492f30c1a8824fc8b6df5058443c9497b07
Phoong Shen Yang <shenyang.zx@gmail.com>
2021-08-05 16:18:33 +0800

    database fix and debug to env

--- chyy/settings.py
+++ chyy/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1','.herokuapp.com']
 
