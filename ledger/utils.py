9af3063df103450f3d4c9b84ff9b351185f51f2f
Phoong Shen Yang <shenyang.zx@gmail.com>
2022-02-14 23:54:10 +0800

    ADAT tabs up and working
    datepicker and separate totals, WIP

--- ledger/utils.py
+++ ledger/utils.py
@@ -5,3 +5,12 @@ def new_external(name):
     other_party = Account(name=name, file_no=f"EXTERNAL{count}", balance=0)
     other_party.save()
     return other_party
+
+# removed after 1 time use
+def categorize_ad():
+    to_cat = [y for y in Transaction.objects.all() if y.payee.is_office()]
+    for each in to_cat:
+        tbl_list = json.loads(each.table_list)
+        if tbl_list[0]['type_code'] == 'AD':
+            each.category = 'AD'
+            each.save()
