172424ceca5ff318951b9151c5907e156820c24f
Phoong Shen Yang <shenyang.zx@gmail.com>
2022-02-22 13:55:46 +0800

    moved reimbursed tab filling into utils

--- ledger/static/ledger/utils.js
+++ ledger/static/ledger/utils.js
@@ -1,9 +1,46 @@
-dateTo_lt_dateFrom = (dateTo,dateFrom) => {
-    let sliDateFrom = dateFrom.slice(-4) + dateFrom.slice(3,5) + dateFrom.slice(0,2)
-    let sliDateTo =  dateTo.slice(-4) + dateTo.slice(3,5) + dateTo.slice(0,2)
-    if (sliDateTo<sliDateFrom){
+date1_lt_date2 = (date1,date2) => {
+    let slidate2 = date2.slice(-4) + date2.slice(3,5) + date2.slice(0,2)
+    let slidate1 =  date1.slice(-4) + date1.slice(3,5) + date1.slice(0,2)
+    if (slidate1<slidate2){
         return true
     } else{
         return false
     }
+}
+
+updateTabs = (dateFrom, dateTo, reimbursed,) =>{
+    let reimbursedTotal = 0
+    for (const [i, trans] of reimbursed.entries()){
+        if (!date1_lt_date2(trans.created_at,dateFrom) && date1_lt_date2())
+        for (const each of trans.table_list){
+            reimbursedTotal+= Number(each.amount)
+            $('#reimbursedBody').append(
+                `<tr class="transRow">
+                <td class="refCell">
+                </td>
+                <td class="dateCell">
+                    ${trans.created_at}
+                </td>
+                <td>${trans.category}</td>
+                <td>
+                    ${each.description}
+                </td>
+                <td>
+                    ${trans.receiver}
+                </td>
+                <td class="text-end">${each.amount}</td>
+            </tr>`
+            )
+        }
+    }
+    $('#reimbursedBody').append(`
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="text-end fw-bold">Total</td>
+            <td class="text-end fw-bold">${reimbursedTotal.toFixed(2)}</td>
+        </tr>
+    `)
 }
